85ecc3ddc005f47331aafd897a32437a62f53b37
[YanaB] <[tudabae@gmail.com]>
2023-07-24 16:00:30 +0300

    сумма чисел от м до н

---
+++ zd66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Console.Write("enter number M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("enter number N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма чисел от {m} до {n} равна {SumNumbers(m, n)}");
+int SumNumbers(int start, int end){
+    if(start == end) return start;
+    return (start + SumNumbers(start + 1, end));
+}
